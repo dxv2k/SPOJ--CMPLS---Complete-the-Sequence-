ff5fb32651514e46857f2228ea7ddf014255ab10
dxv2k <9569076+dxv2k@users.noreply.github.com>
2019-02-12 17:15:32 +0700

    Add files via upload

---
+++ cmpls.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+      //P(n) = aD.nD+aD-1.nD-1+...+a1.n+a0
+      //  If aD <> 0, the number D is called a degree of the polynomial.
+      // Note that constant function P(n) = C can be considered as
+      // polynomial of degree 0,
+      // and the zero function P(n) = 0 is
+      // usually defined to have degree -1.
+
+
+}
